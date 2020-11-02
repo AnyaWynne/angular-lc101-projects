b49ebd11c1ada662f174a721e33417ce00f9ba64
Anya Wynne <avkwynne@gmail.com>
2020-11-02 14:15:33 -0600

    finished first part

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Places to Visit';
+  image1 = 'https://explorestlouis.com/wp-content/uploads/2013/03/Citygarden.jpg';
+  image2 = 'https://media.tacdn.com/media/attractions-splice-spp-674x446/09/50/15/ff.jpg';
+  image3 = 'https://media2.fdncms.com/riverfronttimes/imager/u/original/2568521/laumeier11.jpg';
 
   constructor() { }
 
